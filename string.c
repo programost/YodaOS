52391756bc1bfd983440f7aa7dab19f97718b12d
programost <166943310+programost@users.noreply.github.com>
2026-04-10 08:46:16 +0000

    Update string.c

--- string.c
+++ string.c
@@ -1,52 +1,4 @@
-#include <stddef.h>
-
-size_t strlen(const char *s) {
-    size_t len = 0;
-    while (s[len]) len++;
-    return len;
-}
-
-int strcmp(const char *s1, const char *s2) {
-    while (*s1 && (*s1 == *s2)) { s1++; s2++; }
-    return *(unsigned char*)s1 - *(unsigned char*)s2;
-}
-
-int strncmp(const char *s1, const char *s2, size_t n) {
-    while (n && *s1 && (*s1 == *s2)) { s1++; s2++; n--; }
-    if (n == 0) return 0;
-    return *(unsigned char*)s1 - *(unsigned char*)s2;
-}
-
-char *strcpy(char *dest, const char *src) {
-    char *d = dest;
-    while ((*d++ = *src++));
-    return dest;
-}
-
-char *strncpy(char *dest, const char *src, size_t n) {
-    char *d = dest;
-    size_t i;
-    for (i = 0; i < n && src[i]; i++) d[i] = src[i];
-    for (; i < n; i++) d[i] = '\0';
-    return dest;
-}
-
-char *strcat(char *dest, const char *src) {
-    char *d = dest;
-    while (*d) d++;
-    while ((*d++ = *src++));
-    return dest;
-}
-
-char *strstr(const char *haystack, const char *needle) {
-    if (!*needle) return (char*)haystack;
-    for (; *haystack; haystack++) {
-        const char *h = haystack, *n = needle;
-        while (*h && *n && (*h == *n)) { h++; n++; }
-        if (!*n) return (char*)haystack;
-    }
-    return NULL;
-}
+#include "string.h"
 
 void *memset(void *s, int c, size_t n) {
     unsigned char *p = s;
@@ -72,4 +24,51 @@ void *memmove(void *dest, const void *src, size_t n) {
         while (n--) *--d = *--s;
     }
     return dest;
+}
+
+size_t strlen(const char *s) {
+    size_t len = 0;
+    while (*s++) len++;
+    return len;
+}
+
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s1 == *s2) { s1++; s2++; }
+    return *(unsigned char*)s1 - *(unsigned char*)s2;
+}
+
+int strncmp(const char *s1, const char *s2, size_t n) {
+    while (n && *s1 && *s1 == *s2) { s1++; s2++; n--; }
+    if (n == 0) return 0;
+    return *(unsigned char*)s1 - *(unsigned char*)s2;
+}
+
+char *strcpy(char *dest, const char *src) {
+    char *d = dest;
+    while ((*d++ = *src++));
+    return dest;
+}
+
+char *strncpy(char *dest, const char *src, size_t n) {
+    char *d = dest;
+    while (n && (*d++ = *src++)) n--;
+    while (n--) *d++ = '\0';
+    return dest;
+}
+
+char *strcat(char *dest, const char *src) {
+    char *d = dest;
+    while (*d) d++;
+    while ((*d++ = *src++));
+    return dest;
+}
+
+char *strstr(const char *haystack, const char *needle) {
+    size_t nlen = strlen(needle);
+    while (*haystack) {
+        if (strncmp(haystack, needle, nlen) == 0)
+            return (char*)haystack;
+        haystack++;
+    }
+    return 0;
 }
